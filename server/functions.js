e502bfd49c9176892729a415c4c4b8e58fa660c3
emiliojroybal <emiliojroybal@gmail.com>
2026-08-26 13:23:07 -0700

    Initialized frontend

--- server/functions.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-const idList = [];
-
-export const generateID = (type) => {
-  let newID = crypto.randomBytes(4).toString('hex');
-  while (idList.indexOf(newID) >= 0) newID = crypto.randomBytes(4).toString('hex');
-  idList.push(`${type}${newID}`);
-  return `${type}${newID}`;
-}
