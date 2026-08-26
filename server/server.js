8331130c7ce874d5277f53267ac14d0ed5ef7009
emiliojroybal <emiliojroybal@gmail.com>
2026-08-26 12:14:15 -0700

    Added post functionality to API server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'development']);
+const { generateID } = require('./functions');
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,6 +20,23 @@ app.get("/:dataType", (req, res) => {
     .catch(err => res.status(400).json({message: err}));
 })
 
+app.post("/:dataType", async (req, res) => {
+    const dataType = req.params.dataType;
+    const body = req.body;
+    console.log(req.body);
+    const typePrefix = await knex("type_prefix").select("prefix").where({type: dataType}).first();
+    const newID = generateID(typePrefix.prefix);
+    const insertion = {
+        ...body,
+        id: newID,
+    }
+    
+    console.log(`Trying to insert ${insertion.name} to ${dataType}`);
+
+    knex(dataType).insert(insertion)
+    .then(data => res.status(200).json({message: `Successfully inserted ${insertion.name} with ID ${insertion.id}`}))
+    .catch(err => res.status(400).json({message: err}));
+})
 
 app.listen(PORT, () => {
     console.log("Server listening on port " + PORT);
